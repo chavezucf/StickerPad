f2a81a4d3fff09c9c4506c9c12c1c5aca1b2c04c
Miguel <miguel@miguelchavezweb.com>
2023-08-05 17:22:21 -0400

    Initial Commit

---
+++ StickerPad/StickerPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerPadApp.swift
+//  StickerPad
+//
+//  Created by Miguel Chavez on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct StickerPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
